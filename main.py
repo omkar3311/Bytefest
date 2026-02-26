45d40312afa6299bb536ccd5ccfae00bd0d9f27b
omkar3311 <omkarwaghmare3311@gmail.com>
2026-02-26 08:42:04 +0530

    supabase

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+from supabase import create_client
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+SUPABASE_URL = os.getenv("supabase_url")
+SUPABASE_KEY = os.getenv("supabase_key")
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)    
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -22,9 +29,10 @@ class LoadRequest(BaseModel):
     secret_code: str
 
 class SubmitRequest(BaseModel):
+    secret_code: str
     user_code: str
     time_taken: int
-
+    
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
@@ -37,11 +45,17 @@ def load_code(data: LoadRequest):
     if data.secret_code != "123":
         return {"success": False, "message": "Invalid secret code"}
 
+    supabase.table("participants").upsert({
+        "name": data.name,
+        "code": data.secret_code
+    }).execute()
+    
     elements = DEFAULT_CODE.replace("\n", " \n ").split(" ")
     elements = [e for e in elements if e.strip()]
 
     return {
         "success": True,
+        "user_id": data.secret_code,
         "code_elements": elements,
         "time_limit": 120
     }
@@ -55,6 +69,9 @@ def submit_code(data: SubmitRequest):
     for i in range(min(len(correct_lines), len(user_lines))):
         if correct_lines[i].strip() == user_lines[i].strip():
             score += 1
+    result = supabase.table("participants").update({
+        "score": score
+    }).eq("code", data.secret_code).execute()
 
     return {
         "success": True,
