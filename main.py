62e51cd7fb6f47d6f27c23db19d09e5c24df4821
omkar3311 <omkarwaghmare3311@gmail.com>
2026-02-26 18:27:16 +0530

    defualt codes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from supabase import create_client
 from dotenv import load_dotenv
 import os
+from utility import DEFAULT_CODES
 
 load_dotenv()
 SUPABASE_URL = os.getenv("supabase_url")
@@ -16,13 +17,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-DEFAULT_CODE = """#include <stdio.h>
+# DEFAULT_CODE = """#include <stdio.h>
 
-int main() {
-    printf("Hello, Bytefest!");
-    return 0;
-}
-"""
+# int main() {
+#     printf("Hello, Bytefest!");
+#     return 0;
+# }
+# """
 
 class LoadRequest(BaseModel):
     name : str
@@ -42,15 +43,22 @@ def load_code(data: LoadRequest):
     print("Received:", data)
     if not data.name.strip():
         return {"success": False, "message": "Name is required"}
-    if data.secret_code != "123":
+    if data.secret_code not in DEFAULT_CODES:
         return {"success": False, "message": "Invalid secret code"}
-
+    
+    result = supabase.table("participants").select("code").eq("code", data.secret_code).execute().data
+    if result:
+        return {
+            "success": False,
+            "message": "This secret code has already been used"
+        }
+        
     supabase.table("participants").upsert({
         "name": data.name,
         "code": data.secret_code
     }).execute()
     
-    elements = DEFAULT_CODE.replace("\n", " \n ").split(" ")
+    elements = DEFAULT_CODES[data.secret_code].replace("\n", " \n ").split(" ")
     elements = [e for e in elements if e.strip()]
 
     return {
@@ -62,14 +70,14 @@ def load_code(data: LoadRequest):
 
 @app.post("/submit-code")
 def submit_code(data: SubmitRequest):
-    correct_lines = DEFAULT_CODE.strip().splitlines()
+    correct_lines = DEFAULT_CODES[data.secret_code].strip().splitlines()
     user_lines = data.user_code.strip().splitlines()
-
+    
     score = 0
     for i in range(min(len(correct_lines), len(user_lines))):
         if correct_lines[i].strip() == user_lines[i].strip():
             score += 1
-    result = supabase.table("participants").update({
+    supabase.table("participants").update({
         "score": score
     }).eq("code", data.secret_code).execute()
 
